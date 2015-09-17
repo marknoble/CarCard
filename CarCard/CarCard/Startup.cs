9454bee50859c6d82002ca84e29118f8e67c2205
Mark Noble <mark.noble@live.com>
2015-09-17 12:37:13 -0400

    Created base MVC project with Knockoutjs

---
+++ CarCard/CarCard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CarCard.Startup))]
+namespace CarCard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
